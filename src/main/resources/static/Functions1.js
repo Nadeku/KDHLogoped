0c50a23a36aa95357bc6b34d6ba77a6fc8790754
s360774 <s360774@oslomet.no>
2024-02-29 09:27:24 +0100

    Erase old menu dropdown. Add underline a:hover

--- src/main/resources/static/Functions1.js
+++ src/main/resources/static/Functions1.js
@@ -8,23 +8,3 @@ function menuButton() {
     x.className = "topnav";
   }
 }
-
-// On dropdown menu click, show content
-function dropdownMenu(){
-    document.getElementById("myDropdown").classList.toggle("show");
-}
-
-window.onclick = function(event) {
-
-    // Hide dropdown menu bar if clicked outside the menu
-    if (!event.target.matches('.dropbtn')) {
-      var dropdowns = document.getElementsByClassName("dropdown-content");
-      var i;
-      for (i = 0; i < dropdowns.length; i++) {
-        var openDropdown = dropdowns[i];
-        if (openDropdown.classList.contains('show')) {
-          openDropdown.classList.remove('show');
-        }
-      }
-    }
-  }
