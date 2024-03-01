4efe6eb25238fc256e381b1a2f17dee094cb0b3e
s360774 <s360774@oslomet.no>
2024-03-01 23:34:13 +0100

    New better menu! add oldmenu.html to rep

--- src/main/resources/static/Functions1.js
+++ src/main/resources/static/Functions1.js
@@ -1,10 +1,20 @@
 
-/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
-function menuButton() {
-  var x = document.getElementById("myTopnav");
-  if (x.className === "topnav") {
+
+/* Toggle between adding and removing the "responsive" class to navbar from menu click in small screen*/
+function menuToggle() {
+  var x = document.getElementById("navbar");
+  if (x.className === "navbar") {
     x.className += " responsive";
   } else {
-    x.className = "topnav";
+    x.className = "navbar";
   }
 }
+
+/* Remove responsive class on window resize*/
+function removeResponsiveClass(){
+  var x = document.getElementById("navbar");
+  x.classList.remove("responsive");
+    x.className = "navbar";
+}
+
+window.addEventListener("resize", removeResponsiveClass);
