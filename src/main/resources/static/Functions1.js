aae4b0c4d84d67e250c8065d2aca1934a7e58dc8
s360774 <s360774@oslomet.no>
2024-03-17 15:32:23 +0100

    add new changes to original files. delete experimental

--- src/main/resources/static/Functions1.js
+++ src/main/resources/static/Functions1.js
@@ -1,29 +1,60 @@
 
 
-/* HIDE SHOW MOBILE MENU navbar2*/
-function openNav() {
-  document.getElementById("nav").style.display = "block";
+/* Hide/show back to top button */
+window.addEventListener("scroll", backtotop);
+
+function backtotop() {
+  var btn = document.getElementsByClassName("topBtn")[0];
+  var scrollHeight = 200;
+  if (window.scrollY > scrollHeight) {
+    btn.style.display = 'block';
+  } else {
+    btn.style.display = 'none';
+  }
+
 }
-
-function closeNav() {
-  document.getElementById("nav").style.display = "none";
-}
-
-
 
 
 /* Toggle between adding and removing the responsive class
- to navbar from menu click in small screen*/
+ to navbar from menu click in small screen
+ and change toggle button*/
 function menuToggle() {
   var x = document.getElementById("navbar");
+  var toggleIcon = document.getElementById("toggle");
+
+  /* Endre responsive class */
   if (x.className === "navbar") {
     x.className += " responsive";
+
+
+    /* Move screen to navbar*/
+    var target = document.querySelector('nav');
+    var headerHeight = document.querySelector('header').offsetHeight;
+    var extragap = 10;
+    var targetPosition = target.offsetTop - headerHeight - extragap;
+    window.scrollTo({
+      top: targetPosition,
+      behavior: "smooth"
+    });
+
+    /* Change icon */
+    toggleIcon.classList.remove("fa-bars");
+    toggleIcon.classList.add("fa-xmark");
+
+
   } else {
     x.className = "navbar";
+
+    /* Change icon */
+    toggleIcon.classList.remove("fa-xmark");
+    toggleIcon.classList.add("fa-bars");
   }
+
+
+
 }
 
-/* Make sure responsive class is not present if screen >= 600px*/
+/* Make sure responsive class is not present if screen >= 600px */
 function removeResponsiveClass() {
   var x = document.getElementById("navbar");
   if (x.classList.contains("responsive") && window.innerWidth >= 600) {
